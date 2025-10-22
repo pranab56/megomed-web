a7eb84fa99a15f955f80f55fb24a69406e12503d
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-10-22 17:07:34 +0600

    base url

--- src/utils/BaseURL.js
+++ src/utils/BaseURL.js
@@ -1,3 +1,3 @@
-export const baseURL = "http://10.10.7.65:5006";
+// export const baseURL = "http://10.10.7.65:5006";
 
-// export const baseURL = "http://82.165.134.157:5000";
+export const baseURL = "http://82.165.134.157:5000";
